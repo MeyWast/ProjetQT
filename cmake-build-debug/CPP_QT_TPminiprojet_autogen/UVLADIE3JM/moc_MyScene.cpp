4df087869affca5e6da8a1739b1764bd133c29bf
Alexandre <51417462+MeyWast@users.noreply.github.com>
2023-01-18 11:22:17 +0100

    Add files via upload

---
+++ cmake-build-debug/CPP_QT_TPminiprojet_autogen/UVLADIE3JM/moc_MyScene.cpp
@@ -0,0 +1,139 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'MyScene.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.11.3)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../src/MyScene.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'MyScene.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.11.3. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MyScene_t {
+    QByteArrayData data[9];
+    char stringdata0[55];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MyScene_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MyScene_t qt_meta_stringdata_MyScene = {
+    {
+QT_MOC_LITERAL(0, 0, 7), // "MyScene"
+QT_MOC_LITERAL(1, 8, 6), // "update"
+QT_MOC_LITERAL(2, 15, 0), // ""
+QT_MOC_LITERAL(3, 16, 5), // "temps"
+QT_MOC_LITERAL(4, 22, 5), // "spawn"
+QT_MOC_LITERAL(5, 28, 4), // "quit"
+QT_MOC_LITERAL(6, 33, 9), // "setpseudo"
+QT_MOC_LITERAL(7, 43, 4), // "save"
+QT_MOC_LITERAL(8, 48, 6) // "newtry"
+
+    },
+    "MyScene\0update\0\0temps\0spawn\0quit\0"
+    "setpseudo\0save\0newtry"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MyScene[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   49,    2, 0x0a /* Public */,
+       3,    0,   50,    2, 0x0a /* Public */,
+       4,    0,   51,    2, 0x0a /* Public */,
+       5,    0,   52,    2, 0x0a /* Public */,
+       6,    0,   53,    2, 0x0a /* Public */,
+       7,    0,   54,    2, 0x0a /* Public */,
+       8,    0,   55,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MyScene::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MyScene *_t = static_cast<MyScene *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->update(); break;
+        case 1: _t->temps(); break;
+        case 2: _t->spawn(); break;
+        case 3: _t->quit(); break;
+        case 4: _t->setpseudo(); break;
+        case 5: _t->save(); break;
+        case 6: _t->newtry(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+QT_INIT_METAOBJECT const QMetaObject MyScene::staticMetaObject = {
+    { &QGraphicsScene::staticMetaObject, qt_meta_stringdata_MyScene.data,
+      qt_meta_data_MyScene,  qt_static_metacall, nullptr, nullptr}
+};
+
+
+const QMetaObject *MyScene::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MyScene::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MyScene.stringdata0))
+        return static_cast<void*>(this);
+    return QGraphicsScene::qt_metacast(_clname);
+}
+
+int MyScene::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QGraphicsScene::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 7;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
